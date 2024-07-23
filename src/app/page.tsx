67d9383db3ed7d58b702fc6c230c0e1287f9464d
AdnanDev05 <adnanarodiya.techrizz@gmail.com>
2024-07-23 17:27:02 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import AboutStart from "@/components/about-start";
+import ClassStarts from "@/components/class-starts";
 import MainCarousel from "@/components/main-carousel";
+import SchoolFacilities from "@/components/school-facilities";
+import TeamStart from "@/components/team-start";
 
 export default function Home() {
   return (
     <>
       <MainCarousel />
+      <SchoolFacilities />
+      <AboutStart />
+      <ClassStarts />
+      <TeamStart />
     </>
   );
 }
